fe0fa14ea7c3bf7816c7783fba46100e5bfb82d2
지현 <jh0neee@naver.com>
2022-12-07 01:44:11 +0900

    nav side Drawer 애니메이션 추가

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -4,17 +4,17 @@ import { NavLink } from 'react-router-dom';
 import './NavLinks.css';
 
 const NavLinks = (props) => {
-    return <ul className='nav-links'>
-        <li>
+    return <ul className='nav-links' >
+        <li onClick={props.onClick}>
             <NavLink to='/' exact>Home</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClick}>
             <NavLink to='/survey' exact>Reserch</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClick}>
             <NavLink to='/survey/request'>설문요청</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClick}>
             <NavLink to='/auth'>로그인</NavLink>
         </li>
     </ul>;
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
-import './SideDrawer.css';
+import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
-    const drawerContent = <aside className='side-drawer'>{props.children}</aside>
-
-    return ReactDOM.createPortal(drawerContent, document.getElementById('drawer'))
+  const drawerContent = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer">{props.children}</aside>
+    </CSSTransition>
+  );
+  return ReactDOM.createPortal(
+    drawerContent,
+    document.getElementById("drawer")
+  );
 };
 
 export default SideDrawer;
