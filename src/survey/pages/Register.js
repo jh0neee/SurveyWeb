1e967b80f916e159e2d33cc1cc929c6931c91402
지현 <jh0neee@naver.com>
2023-03-23 03:08:11 +0900

    survey_Register_surveyInput 상태관리

--- src/survey/pages/Register.js
+++ src/survey/pages/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useReducer, useCallback } from "react";
 
 import Dropdown from "../../shared/components/FormElements/Dropdown";
 import Button from "../../shared/components/FormElements/Button";
@@ -7,63 +7,107 @@ import Check from "../components/Check";
 
 import "../styles/Register.css";
 
-const Register = () => {
-  const [selectedOpt, setSelectedOpt] = useState("");
-  const [questionItem, setQusetionItem] = useState([]); // 질문 value
-  const [checkOpt, setCheckOpt] = useState([]); // 체크박스 옵션
-  const shortAnswerTypeId = useRef(0); // 단답형 질문 객체 Id
-  const subjectiveTypeId = useRef(0); // 주관형 질문 객체 Id
-
-  const singleQuestion = {
-    // 객관식 주관형 질문 객체
-    id: "s" + subjectiveTypeId.current,
-    selectedOpt,
-    question: "",
-  };
-
-  const checkQuestion = {
-    // 체크박스 질문 객체
-    id: "c" + shortAnswerTypeId.current,
-    selectedOpt,
+const initialState = {
+  inputs: {
+    id: "",
+    selectOption: "",
     question: "",
     option: [],
-  };
+  },
+  questionItem: [
+    {
+      id: "s0",
+      selectedOpt: "객관식",
+      question: "안녕하세요",
+    },
+  ],
+};
+const surveyReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        questionItem: state.questionItem.map((item) =>
+          item.id === action.payload ? { ...item, question: action.val } : item
+        ),
+      };
+    case "CREATE_SURVEY":
+      return {
+        inputs: initialState.inputs,
+        questionItem: state.questionItem.concat(action.questionItem),
+      };
+    case "DELETE_SURVEY":
+      return {
+        ...state,
+        questionItem: state.questionItem.filter(
+          (item) => item.id !== action.payload
+        ),
+      };
 
-  const addListHandler = () => {
-    if (selectedOpt === "체크박스") {
-      setQusetionItem([...questionItem, checkQuestion]);
+    default:
+      return state;
+  }
+};
+
+const Register = () => {
+  const [selectedOpt, setSelectedOpt] = useState("");
+  const [state, dispatch] = useReducer(surveyReducer, initialState);
+  const [checkOpt, setCheckOpt] = useState([]); // 체크박스 옵션
+  const shortAnswerTypeId = useRef(1); // 단답형 질문 객체 Id
+  const subjectiveTypeId = useRef(1); // 주관형 질문 객체 Id
+
+  const { questionItem } = state;
+  const { question, option } = state.inputs;
+
+  const onChangeInput = useCallback((e, id) => {
+    dispatch({
+      type: "CHANGE_INPUT",
+      val: e.target.value,
+      payload: id,
+    });
+  }, []);
+
+  const addListHandler = useCallback(() => {
+    if (selectedOpt !== "체크박스") {
+      dispatch({
+        type: "CREATE_SURVEY",
+        questionItem: {
+          id: "s" + shortAnswerTypeId.current,
+          selectOption: selectedOpt,
+          question,
+        },
+      });
       shortAnswerTypeId.current += 1;
-    } else if (selectedOpt === "") {
-      setQusetionItem([...questionItem]);
-    } else {
-      setQusetionItem([...questionItem, singleQuestion]);
+    } else if (selectedOpt === "체크박스") {
+      dispatch({
+        type: "CREATE_SURVEY",
+        questionItem: {
+          id: "c" + subjectiveTypeId.current,
+          selectOption: selectedOpt,
+          question,
+          option,
+        },
+      });
       subjectiveTypeId.current += 1;
+    } else {
+      return;
     }
-  };
+  }, [selectedOpt, question]);
 
-  const onChangeInput = (e, index) => {
-    if (questionItem !== undefined) {
-      // 질문 배열이 비어있지 않다면
-      let copiedQusetionInput = [...questionItem]; // 배열 안에 있는 질문 객체 복사
-      copiedQusetionInput[index].question = e.target.value;
-      setQusetionItem(copiedQusetionInput);
-    }
-  };
-  
   const surveySubmitHandler = (e) => {
     e.preventDefault();
-    
+
     // checkOpt option 배열을 checkQuestion 객체에 추가
-    checkOpt.forEach(elem => {
-      // checkOpt id와 동일한 id의 questionItem 객체 index를 찾는다.
-      let findIndex = questionItem.findIndex(item => item.id === elem.id);
-      // 기존 배열 복사
-      let copiedItems = [...questionItem];
-      // 복사한 배열에 수정할 값 할당
-      copiedItems[findIndex].option = elem[elem.id];
-      // 수정된 배열 업데이트
-      setQusetionItem(copiedItems);
-    })
+    // checkOpt.forEach((elem) => {
+    //   // checkOpt id와 동일한 id의 questionItem 객체 index를 찾는다.
+    //   let findIndex = questionItem.findIndex((item) => item.id === elem.id);
+    //   // 기존 배열 복사
+    //   let copiedItems = [...questionItem];
+    //   // 복사한 배열에 수정할 값 할당
+    //   copiedItems[findIndex].option = elem[elem.id];
+    //   // 수정된 배열 업데이트
+    //   setQusetionItem(copiedItems);
+    // });
 
     console.log(questionItem);
   };
@@ -78,23 +122,30 @@ const Register = () => {
         <form onSubmit={surveySubmitHandler}>
           <div className="survey-form">
             {questionItem.map((item, index) => (
-              <div key={`survey-question-${item.id}`} className="mid_survey-form">
+              <div
+                key={`survey-question-${item.id}`}
+                className="mid_survey-form"
+              >
                 <Card className="input-card">
                   <div className="delete-btn">
                     <span
                       onClick={() => {
-                        setQusetionItem(questionItem.filter((card) => card.id !== item.id));
+                        dispatch({
+                          type: "DELETE_SURVEY",
+                          payload: item.id,
+                        });
                       }}
                     >
                       ⛔
                     </span>
                   </div>
-                  <div className="select-option">{item.selectedOpt}</div>
-                  {item.selectedOpt === "체크박스" ? (
+                  <div className="select-option">{item.selectOption}</div>
+                  {item.selectOption === "체크박스" ? (
                     <div>
                       <input
+                        placeholder="check"
                         value={item.question}
-                        onChange={(e) => onChangeInput(e, index)}
+                        onChange={(e) => onChangeInput(e, item.id)}
                       />
                       <Check
                         checkQuestionId={item.id}
@@ -102,10 +153,17 @@ const Register = () => {
                         setCheckOpt={setCheckOpt}
                       />
                     </div>
+                  ) : item.selectOption === "객관식" ? (
+                    <input
+                      placeholder="single"
+                      value={item.question}
+                      onChange={(e) => onChangeInput(e, item.id)}
+                    />
                   ) : (
                     <input
+                      placeholder="subject"
                       value={item.question}
-                      onChange={(e) => onChangeInput(e, index)}
+                      onChange={(e) => onChangeInput(e, item.id)}
                     />
                   )}
                 </Card>
