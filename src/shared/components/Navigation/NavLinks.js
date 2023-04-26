5009d775b7f0484d832c56a98efa7c8e1aac7581
지현 <jh0neee@naver.com>
2023-04-26 23:38:40 +0900

    Relocate navigation bar

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -10,22 +10,22 @@ const NavLinks = (props) => {
   return (
     <ul className="nav-links">
       <li onClick={props.onClick}>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">홈</NavLink>
       </li>
       <li onClick={props.onClick}>
-        <NavLink to={"/survey"}>Reserch</NavLink>
+        <NavLink to={"/survey"}>설문하기</NavLink>
       </li>
       <li onClick={props.onClick}>
-        <NavLink to="/request">Request</NavLink>
+        <NavLink to={"/post/new"}>새 글 쓰기</NavLink>
       </li>
       {!auth.isLoggedIn && (
         <li onClick={props.onClick}>
-          <NavLink to="/auth">Login</NavLink>
+          <NavLink to="/auth">로그인</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li onClick={props.onClick}>
-          <button onClick={auth.logout}>Logout</button>
+          <button onClick={auth.logout}>로그아웃</button>
         </li>
       )}
     </ul>
