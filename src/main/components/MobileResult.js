174a64d949b56eed875b15de60b4de0619d6762a
지현 <jh0neee@naver.com>
2023-10-18 22:33:05 +0900

    fix issue preventing navigation to the target path on button click

--- src/main/components/MobileResult.js
+++ src/main/components/MobileResult.js
@@ -1,20 +1,22 @@
 import React from "react";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import SurveyResult from "./SurveyResult";
-// import Button from "../../shared/components/FormElements/Button";
+import Button from "../../shared/components/FormElements/Button";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import "../styles/Mobile.css";
 
 const MobileResult = () => {
-  // const postId = useParams().postId;
+  const postId = useParams.postId();
   return (
     <>
       <div className='result-title'>
-        <ArrowBackIosNewIcon />
+        <Button to={`/${postId}/content`}>
+          <ArrowBackIosNewIcon />
+        </Button>
         <h2>결과</h2>
       </div>
-      <SurveyResult />
+      <SurveyResult postId={postId} />
     </>
   );
 };
